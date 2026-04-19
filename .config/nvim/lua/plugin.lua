3768e7c3300428a6fcf47703978af6241a11c20d
Gioxgio <fantastici02@hotmail.it>
2026-04-19 16:10:48 +0200

    Update git - Update theme

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -1,7 +1,10 @@
-vim.pack.add{
+vim.pack.add {
+	'https://github.com/folke/tokyonight.nvim',
 	'https://github.com/neovim/nvim-lspconfig'
 }
 
+vim.cmd.colorscheme 'tokyonight'
+
 vim.api.nvim_create_user_command('PackUpdate', function()
-  vim.pack.update()
+	vim.pack.update()
 end, {})
