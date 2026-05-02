9919a33d2f35496015b8bad05f15e7ee4e573fe7
Gioxgio <fantastici02@hotmail.it>
2026-05-02 14:10:39 +0200

    Update nvim: Add telescope

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -1,7 +1,27 @@
 vim.pack.add {
 	'https://github.com/folke/tokyonight.nvim',
-	'https://github.com/neovim/nvim-lspconfig'
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/nvim-lua/plenary.nvim',
+	'https://github.com/nvim-telescope/telescope.nvim',
 }
+
+--- Telescope
+
+local telescope = require('telescope')
+telescope.setup({})
+
+local builtin = require('telescope.builtin')
+local function telescope_search()
+	vim.cmd('noau normal! "vy')
+	local text = vim.fn.getreg('v')
+	 builtin.live_grep({ default_text = text })
+end
+
+vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>s', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('v', '<leader>s', telescope_search, { desc = 'Telescope live grep  selection' })
+
+---
 
 vim.cmd.colorscheme 'tokyonight'
 
