549504c02e5817f379d48c2e6b1154658bc25b09
Gioxgio <fantastici02@hotmail.it>
2026-05-09 11:50:32 +0200

    Update git - Made search mappings more intuitive

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -11,15 +11,24 @@ local telescope = require('telescope')
 telescope.setup({})
 
 local builtin = require('telescope.builtin')
-local function telescope_search()
+local function telescope_search_file()
 	vim.cmd('noau normal! "vy')
-	local text = vim.fn.getreg('v')
-	 builtin.live_grep({ default_text = text })
+	local text = vim.fn.getreg('v'):gsub("\n", "")
+	builtin.find_files({ default_text = text })
+end
+local function telescope_search_text()
+	vim.cmd('noau normal! "vy')
+	local text = vim.fn.getreg('v'):gsub("\n", "\\n")
+	builtin.live_grep({
+		additional_args = function() return { "--multiline" } end,
+		default_text = text
+	})
 end
 
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>s', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('v', '<leader>s', telescope_search, { desc = 'Telescope live grep  selection' })
+vim.keymap.set('n', '<leader>o', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('v', '<leader>o', telescope_search_file, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>F', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('v', '<leader>F', telescope_search_text, { desc = 'Telescope live grep selection' })
 
 ---
 
